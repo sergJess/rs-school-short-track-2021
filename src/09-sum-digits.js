83e950a4e7b51efc5995ec3828df1e97c9f08d42
sergJess <serg.fantasy@yandex.ru>
2021-04-09 16:47:01 +0300

    feat: add solution for task sum of digits

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,20 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(number) {
+  if (typeof number === 'number') {
+    let num = number;
+    while (num >= 10) {
+      const str = `${num}`;
+      let temp = 0;
+      for (let i = 0; i < str.length; i++) {
+        temp += (+str[i]);
+      }
+      num = temp;
+    }
+    return num;
+  }
+  return false;
 }
 
 module.exports = getSumOfDigits;
